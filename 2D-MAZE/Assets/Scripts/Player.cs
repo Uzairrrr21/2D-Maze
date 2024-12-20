d28a46d6c2e83ba29bd7722edef56c2f9cbadab1
Uzairrrr21 <uzairsohail503@gmail.com>
2024-12-20 21:42:55 +0500

    final commit

--- 2D-MAZE/Assets/Scripts/Player.cs
+++ 2D-MAZE/Assets/Scripts/Player.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player: MonoBehaviour
 {
+    public int keys = 0;
     public float speed = 5.0f;
+    public GameObject door;
+    public Text keyAmount;
+    public Text youwin;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,10 +37,30 @@ public class Player: MonoBehaviour
         {
             transform.Translate(0, -speed * Time.deltaTime, 0);
         }
+
+        if (keys==3)
+        {
+            Destroy(door);
+        }
     }
    
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.tag== "Keys")
+        {
+            keys++;
+            keyAmount.text = "Keys: " + keys;
+            Destroy(collision.gameObject);
+        }
+        if(collision.gameObject.tag== "Gem")
+        {
+            youwin.text = "You Win!!!!!";
+        }
+        if(collision.gameObject.tag== "Enemies")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);  
+            //Debug.Log("Enemy Hit");
+        }
         if(collision.gameObject.tag== "Walls")
         {
             if (Input.GetKey(KeyCode.LeftArrow))
